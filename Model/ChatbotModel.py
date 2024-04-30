bd5e993a3dca44fc3b623a939abd0f2f2d8a0ac2
jin3811 <hjwoo1028@naver.com>
2024-05-01 00:21:22 +0900

    모델 ai 잘못 적혀있던 것 수정

--- Model/ChatbotModel.py
+++ Model/ChatbotModel.py
@@ -1,22 +1,2 @@
-from transformers import PreTrainedTokenizerFast, BartForConditionalGeneration
-
-tokenizer = PreTrainedTokenizerFast.from_pretrained("ainize/kobart-news")
-model = BartForConditionalGeneration.from_pretrained("ainize/kobart-news")
-
 def getAnswer(question : str):
-    input_ids = tokenizer.encode(question, return_tensors="pt")
-    # Generate Summary Text Ids
-    summary_text_ids = model.generate(
-        input_ids=input_ids,
-        bos_token_id=model.config.bos_token_id,
-        eos_token_id=model.config.eos_token_id,
-        length_penalty=2.0,
-        max_length=142,
-        min_length=56,
-        num_beams=4,
-        no_repeat_ngram_size=2
-    )
-    # Decoding Text
-    return tokenizer.decode(summary_text_ids[0], skip_special_tokens=True)
-
-    # return f"question : {question} 에 대한 챗봇 답변입니다."
+    return f"question : {question} 에 대한 챗봇 답변입니다."
--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def chatQA() :
     return res
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
