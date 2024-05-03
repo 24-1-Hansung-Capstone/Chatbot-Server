862d436376a2cdf46f02eedaf4c5ab27bbd76678
jin3811 <hjwoo1028@naver.com>
2024-05-03 18:39:28 +0900

    뉴스와 블로그 글 추출 로직 작성

--- app.py
+++ app.py
@@ -14,14 +14,33 @@ def indexPage():  # put application's code here
 @app.route('/docSummary', methods=['POST'])
 def doc_summary() :
     query = request.json['result']
-    filtered_query = [
+    # print('{' + f"query : {query}" + '}')
+    # visitkorea는 요약 대상이 아니다.
+    query = [
+        item
+        for item in query
+        if item['category'] != "visitkorea"]
+
+    # 뉴스와 블로그를 골고루 추출한다.
+    news_result = [
         item['esDto']['mainBody']
         for item in query
-        if 'esDto' in item and 'mainBody' in item['esDto']]
+        if 'esDto' in item
+            and 'mainBody' in item['esDto']
+            and item['category'] == 'news']
+    blog_result = [
+        item['esDto']['mainBody']
+        for item in query
+        if 'esDto' in item
+           and 'mainBody' in item['esDto']
+           and item['category'] == 'blog']
+
+    filtered_query = [*news_result[:5], *blog_result[:5]]
+
     print(f"filtered_query : {filtered_query}")
     return make_response(
         json.dumps(
-            DocumentSummaryModel.summary(filtered_query[:7]),
+            DocumentSummaryModel.summary(filtered_query),
             ensure_ascii=False,
             indent=2
         )
